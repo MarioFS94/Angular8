5d61ab8d997a7d7bc810b9d68eeb0a47a014262e
mario <mariofernandes1@gmail.com>
2020-02-26 10:46:27 +0100

    Conectar elemento correo.leido con ngModel

---
+++ src/app/Components/correo/correo.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-correo',
+  templateUrl: './correo.component.html',
+  styleUrls: ['./correo.component.css']
+})
+export class CorreoComponent implements OnInit {
+
+  correo: any;
+  pruebaNgIf: string;
+  numbersWords = ['uno', 'dos', 'tres'];
+
+  constructor() {
+    this.correo = {
+      titulo: "Titulo del email",
+      cuerpo: "Cuerpazo del email",
+      emisor: "emisor@gmail.com",
+      destinatario: "receptor@gmail.com"
+    };
+    this.pruebaNgIf= "Es visible si esVisible es true, ya que *ngIf solo evalua booleanos";
+  }
+
+  ngOnInit() {
+  }
+
+  private esVisible() {
+    let visible = false;
+
+    return visible;
+  }
+}
